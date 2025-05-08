82400cd5da73b24b232a11a4f8474a568149f385
simranjl <90574188+simranjl@users.noreply.github.com>
2025-05-07 22:32:37 -0400

    Delete back-end/models/modelFactory.js

--- back-end/models/modelFactory.js
+++
@@ -1,9 +0,0 @@
-//milestone 6
-
-import { FileTaskModel } from './fileTaskModel.js';
-import { FileStorageModel } from './fileStorageModel.js';
-
-export function getTaskModel(type="task") {
-  if (type == "storage") return FileStorageModel;
-  return FileTaskModel;
-}
