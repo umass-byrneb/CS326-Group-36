b0cfc9fb1c79ca17b4381d3d2e907cf67e3b7c6d
Simran Lekhwani <slekhwani@gmail.com>
2025-05-07 21:48:29 -0400

    new events added

--- front-end/source/eventhub/Events.js
+++ front-end/source/eventhub/Events.js
@@ -11,4 +11,37 @@ export const Events = {
   UnStoreTasksFailure: 'UnStoreTasksFailure',
   SwitchToMainView: 'SwitchToMainView',
   SwitchToSimpleView: 'SwitchToSimpleView',
+
+  //for Storage Page
+  //filtering storage based on tags
+  StorageAddTag: 'StorageAddTag',
+  StorageFilterAddTag: 'StorageFilterAddTag',
+  StorageRemoveTag: 'StorageRemoveTag',
+  StorageFilterRemoveTag: 'StorageFilterRemoveTag',
+
+  //load storage listings
+  LoadStorageListings: 'LoadStorageListings',
+  LoadStorageSuccess: 'LoadStorageSuccess',
+  LoadStorageServer: 'LoadStorageServer',
+  LoadStorageServerSuccess: 'LoadStorageServerSucess',
+  LoadAllListings: 'LoadAllListings',
+  LoadAllListingsSuccess: 'LoadAllListingsSuccess',
+
+  //filters for storage
+  StorageCostFilter: 'StorageCostFilter',
+  StorageTimeFilter: 'StorageTimeFilter',
+  StorageSpaceFilter: 'StorageSpaceFilter', 
+
+  //price filter and new button
+  StoragePriceAscend: 'StoragePriceAscend',
+  StoragePriceDescend: 'StoragePriceDescend',
+  StorageUnfilteredList: 'StorageUnfilteredList',
+
+  //update storage listings (called when item is being upload for sale or is being bought)
+  AddStorageItem: 'AddStorageItem',
+  AddStorageItemSuccess: 'AddStorageItemSucces',
+  RemoveStorageItem: 'RemoveStorageItem',
+  RemoveStorageItemSuccess: 'RemoveStorageItemSucess',
+  UpdateStorageItem: 'UpdateStorageItem',
+  UpdateStorageItemSuccess: 'UpdateStorageItemSucess',
 };
