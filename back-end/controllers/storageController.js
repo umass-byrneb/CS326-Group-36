e56ca4b9500ee8ba82a446dba2e4193e4c294de0
Simran Lekhwani <slekhwani@gmail.com>
2025-05-07 21:42:53 -0400

    Added deleteById functionality for storage items and all function use Storage SQLite model

--- back-end/controllers/storageController.js
+++ back-end/controllers/storageController.js
@@ -1,40 +1,61 @@
 //milestone 6
-import { getTaskModel } from "../models/modelFactory.js";
-
-const StorageModel = getTaskModel("storage");
+import { Storage } from "../models/index.js";
 
 
 export async function getAllListings(_, res, next) {
     try {
-        const listings = await StorageModel.getAll();
-        return res.status(200).json({listings})
+        const listings = await Storage.findAll();
+        return res.status(200).json({listings});
     } catch (err) {
+        console.log("error received: ", err);
         next(err)
     }
 }
 
 export async function addStorageItem(req, res, next) {
     try {
-        const request = {...req.body}
-        if (!request.title) {
-            return res.status(400).json({error: "Title is missing"});
+        const {title, duration, cost, size, contact, description, image, owner, listed} = req.body
+        if (!title || !duration || !image || !size) {
+            return res.status(400).json({error: "All required fields weren't received."});
         }
-        const newitem = await StorageModel.create(request);
+        const newitem = await Storage.create({title, duration, cost, size, contact, description, image, owner, listed});
         return res.status(200).json(newitem);
     } catch (err) {
+        console.log("Err in adding storage item: ", err);
         next(err);
     }
 }
 
 export async function updateStorageItem(req, res, next) {
     try {
-        const itemID = req.body;
-        const updateSucess = await StorageModel.updateById(itemID);
-        if (!updateSucess) {
-            return res.status(400).json({error: "Invalid ID. Failed to update the Storage Listing"});
+        const itemID = parseInt(req.params.id);
+        const { listed } = req.body;
+        if (isNaN(itemID) || typeof listed !== 'boolean') {
+            return res.status(400).json({ error: 'Invalid request.' });
         }
-        return res.status(200).json({message: "Successfully updated the item with its ID."});
+        const [count] = await Storage.update({ listed }, { where: { id:itemID } });
+        if (!count) {
+            return res.status(404).json({ error: 'Storage item not found.' });
+        }
+        const updated = await Storage.findByPk(itemID);
+        return res.status(200).json(updated);
     } catch (err) {
         next(err);
+    }
+}
+
+export async function deleteStorageItemId(req, res, next) {
+    try {
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({error: 'Invalid request.'});
+        }
+        const count = await Storage.destroy({ where: { id }});
+        if (!count) {
+            return res.status(400).json({error: 'Storage Item not found.'});
+        }
+        return res.status(200).end();
+    } catch (error) {
+        next(error);
     }
 }
