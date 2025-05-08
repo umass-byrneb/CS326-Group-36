61aaa284b9bc6f2c77f16cfd1be11040a16e20df
Simran Lekhwani <slekhwani@gmail.com>
2025-05-07 21:43:47 -0400

    Added and modified storage routes to create more endpoints

--- back-end/routes/storageRoutes.js
+++ back-end/routes/storageRoutes.js
@@ -1,12 +1,13 @@
 //milestone 6
 
 import express from 'express';
-import { getAllListings, addStorageItem, updateStorageItem } from '../controllers/storageController.js';
+import { getAllListings, addStorageItem, updateStorageItem, deleteStorageItemId } from '../controllers/storageController.js';
 
 const router = express.Router();
 
 router.get('/listings',      getAllListings);
 router.post('/listings',     addStorageItem);
-router.post('/listings/:id', updateStorageItem);
+router.put('/listings/:id', updateStorageItem);
+router.delete('/listings/:id', deleteStorageItemId);
 
 export default router;
